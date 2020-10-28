7b642d6fd44684db29b01f3950ad518195db2955
Minn Maw Oo <minmawoo.ucsm@gmail.com>
2020-10-29 01:52:19 +0630

    setting post routes

--- server/routes/post.js
+++ server/routes/post.js
@@ -4,7 +4,34 @@ const mongoose = require("mongoose");
 const requireLogin = require('../middleware/requireLogin')
 const Post = mongoose.model("Post")
 
+//---------------------------------------------------------------//
+//myPost route
+router.get('/myPosts',requireLogin, async (req,res)=>{
+    let query = Post.find({postedBy:req.user.id})
+    .populate("postedBy", "id name")
+    try {
+        const myPosts = await query.exec()
+        res.json({myPosts})
+    } catch (error) {
+        console.log(error);
+    }
+})
 
+//---------------------------------------------------------------//
+//get post
+router.get('/allPosts',async (req,res)=>{
+    let query = Post.find()
+    .populate("postedBy", "id name")
+    try {
+        const posts = await query.exec()
+        res.json({posts})
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+//---------------------------------------------------------------//
+//post posts
 router.post('/createPost',requireLogin, (req,res)=>{
     const {title, body} = req.body
     if(!title || !body){
@@ -12,10 +39,11 @@ router.post('/createPost',requireLogin, (req,res)=>{
     }
     // console.log(req.user)
     // res.send('ok')
+    req.user.password = undefined
     const post = new Post({
         title,
         body,
-        postedBy: req.user.id
+        postedBy: req.user
     })
     post.save()
     .then(result=>{
