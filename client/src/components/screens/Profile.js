9362cc6dd4543e03499b965b04f66759bd2c6062
Minn Maw Oo <minmawoo.ucsm@gmail.com>
2020-10-29 15:18:52 +0630

    front-end-profile

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
 
 export default function Profile() {
-    return (
-        <h1>Profile</h1>
-    )
+  return (
+    <div style={{maxWidth: "550px", margin: "0px auto"}}>
+      <div style={{
+          display: "flex",
+          justifyContent: 'space-around',
+          margin: "18px 0px",
+          borderBottom: "1px solid grey"
+        }}>
+        <div>
+          <img
+            style={{ width: "160px", height: "160px", borderRadius: "80px" }}
+            src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          />
+        </div>
+        <div>
+          <h4>Minn Maw Oo</h4>
+            <div style={{display: "flex", justifyContent: "space-between", width: "108%"}}>
+                <h6>40 posts</h6>
+                <h6>40 followers</h6>
+                <h6>40 following</h6>
+            </div>
+        </div>
+      </div>
+        <div className="gallery">
+            <img className="item" src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            <img className="item" src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            <img className="item" src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            <img className="item" src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            <img className="item" src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            <img className="item" src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+        </div>
+    </div>
+  );
 }
