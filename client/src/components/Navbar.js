6047a3d8c8ce586e852e726f41c7886a3397ee7d
Minn Maw Oo <minmawoo.ucsm@gmail.com>
2020-10-31 00:41:28 +0630

    changing nevigation bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,39 @@
-import React from "react";
-import {Link} from 'react-router-dom'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../App";
 
 export default function Navbar() {
+  const { state, dispatch } = useContext(UserContext);
+  const renderList = () => {
+    if (state) {
+      return [
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>,
+        <li>
+          <Link to="/create">Create Post</Link>
+        </li>,
+      ];
+    } else {
+      return [
+        <li>
+          <Link to="/signin">Login</Link>
+        </li>,
+        <li>
+          <Link to="/signup">SingUp</Link>
+        </li>,
+      ];
+    }
+  };
+
   return (
     <nav>
-      <div className="nav-wrapper white" style={{color: "black"}}>
-        <Link to="/" className="brand-logo left">
+      <div className="nav-wrapper white" style={{ color: "black" }}>
+        <Link to={state? "/" : "/signin"} className="brand-logo left">
           Instagram
         </Link>
         <ul id="nav-mobile" className="right">
-          <li>
-            <Link to="/signin">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">SingUp</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/create">Create Post</Link>
-          </li>
+          {renderList()}
         </ul>
       </div>
     </nav>
