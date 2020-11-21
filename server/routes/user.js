7be716f282e1d9375969ed3a78ab303ff9cea8cb
Minn Maw Oo <minmawoo.ucsm@gmail.com>
2020-11-21 23:30:59 +0630

    follow and unfollow API

--- server/routes/user.js
+++ server/routes/user.js
@@ -23,5 +23,53 @@ router.get('/user/:id',requireLogin, (req, res)=>{
     })
 })
 
+//--------------------follower && following----------------//
+router.put('follow', (req, res)=>{
+    User.findByIdAndUpdate(req.body.followId, {
+        $push:{followers: req.user._id}
+    },
+    {
+        new: true
+    },(err, result=>{
+        if(err){
+            return res.status(422).json({error: err})
+        }
+        User.findByIdAndUpdate(req.user._id, {
+            $push:{following: req.body.followId}
+        },{new: true})
+        .then(result=>{
+            res.json(result)
+        })
+        .catch(err=>{
+            return res.status(422).json({error: err})
+        })
+    })
+    )
+})
+
+//---------------------unfollowing--------------------//
+router.put('unfollow', (req, res)=>{
+    User.findByIdAndUpdate(req.body.unfollowId, {
+        $push:{followers: req.user._id}
+    },
+    {
+        new: true
+    },(err, result=>{
+        if(err){
+            return res.status(422).json({error: err})
+        }
+        User.findByIdAndUpdate(req.user._id, {
+            $push:{following: req.body.unfollowId}
+        },{new: true})
+        .then(result=>{
+            res.json(result)
+        })
+        .catch(err=>{
+            return res.status(422).json({error: err})
+        })
+    })
+    )
+})
+
 
 module.exports = router
