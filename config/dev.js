6aa48353bfa4015a43027efb016222cf3175dee7
Minn Maw Oo <minmawoo.ucsm@gmail.com>
2020-11-23 02:03:51 +0630

    change directory to git

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    DATABASE_URL: "mongodb+srv://minnmaw:6xVPUr3Wq5E6Suhg@cluster0.wkfw0.mongodb.net/instagram?retryWrites=true&w=majority",
+    JWT_SECRET: "eroehp35049tvm4d"
+}
